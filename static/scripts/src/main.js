180ae6957389c9c45ddede4a66141320adc5d48f
amekusa <hello@amekusa.com>
2020-11-12 23:02:02 +0900

    Close the sidebar menu automatically when user clicked one of the menu items

--- static/scripts/src/main.js
+++ static/scripts/src/main.js
@@ -1,4 +1,23 @@
-function main() {
+/**
+ * @param {string} query
+ * @param {int} index
+ * @return {NodeList|Element|null}
+ */
+function q(query, index = null) {
+	let items = document.querySelectorAll(query);
+	if (index == null) return items;
+	if ((index+1) > items.length) return null;
+	return items[index];
 }
 
-export default main;
+document.addEventListener('DOMContentLoaded', () => {
+	// close the sidebar menu
+	// when user clicked one of the menu items
+	q('.sidebar a').forEach(a => {
+		a.addEventListener('click', ev => {
+			let checkbox = q('input#docolatte-shows-sidebar', 0);
+			if (!checkbox) return;
+			if (checkbox.checked) checkbox.click();
+		});
+	});
+});

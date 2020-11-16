5a32ce94bdcc4a07f75022f7be90b2410e8697c6
amekusa <hello@amekusa.com>
2020-11-16 09:59:15 +0900

    Update the pages, Refactor the code

--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,8 @@
+/**
+ * Automatically takes screenshots using a headless browser
+ * @author Satoshi Soma (amekusa.com)
+ */
+
 const puppeteer = require('puppeteer-core');
 
 (async () => {
@@ -7,26 +12,50 @@ const puppeteer = require('puppeteer-core');
 		args: ['--start-maximized']
 	});
 	let ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36';
+	console.log(`Taking screenshots using`, await browser.version(), '...');
 
 	// pages to go
 	let base = 'file://' + __dirname + '/fixtures-doc/';
 	let pages = [
 		{
+			url: base + 'index.html',
+			viewport: [1024, 768],
+			scroll: [0, 950],
+			saveAs: 'home.png'
+		}, {
+			url: base + 'module-mixins_signalable-Signal.html',
+			viewport: [1024, 640],
+			scroll: [0, 0],
+			saveAs: 'class.png'
+		}, {
 			url: base + 'module-ink_collector.html',
 			viewport: [1024, 640],
 			scroll: [0, 300],
-			saveAs: 'test-desktop.png'
+			saveAs: 'methods.png'
 		}, {
-			url: base + 'module-ink_collector.html',
+			url: base + 'module-base.html',
+			viewport: [1024, 640],
+			saveAs: 'footer.png'
+		}, {
+			url: base + 'documents_collector.js.html#line7',
+			viewport: [1024, 640],
+			saveAs: 'source.png'
+		}, {
+			url: base + 'base_chains.html',
 			viewport: [375, 667],
-			scroll: [0, 200],
-			saveAs: 'test-mobile.png'
+			saveAs: 'mobile.png'
+		}, {
+			url: base + 'base_chains.html',
+			viewport: [375, 667],
+			click: '.menu-button',
+			saveAs: 'mobile-menu.png'
 		}
 	];
-	let saveTo = __dirname + '/gallery/';
 
-	// process the pages
+	// process each page
 	for (let i of pages) {
+		if (!i.scroll) i.scroll = [0, 0];
+
 		let page = await browser.newPage();
 		await page.setUserAgent(ua);
 		await page.setViewport({
@@ -39,17 +68,26 @@ const puppeteer = require('puppeteer-core');
 			waitUntil: 'networkidle0',
 			timeout: 60000
 		});
+
 		// scroll
-		await page.evaluate(i => {
-			window.scrollTo({
-				left: i.scroll[0],
-				top:  i.scroll[1],
-				behavior: 'auto'
-			});
-			Promise.resolve();
-		}, i);
-		// wait for scroll
-		await page.waitForTimeout(1000);
+		if (i.scroll) {
+			await page.evaluate(i => {
+				window.scrollTo({
+					left: i.scroll[0],
+					top:  i.scroll[1],
+					behavior: 'auto'
+				});
+				Promise.resolve();
+			}, i);
+			// wait for scroll
+			await page.waitForTimeout(1000);
+		}
+
+		// click
+		if (i.click) {
+			await page.click(i.click);
+			await page.waitForTimeout(1000);
+		}
 
 		// compute clipping area
 		if (!i.clip) i.clip = { x: 0, y: 0 };
@@ -59,15 +97,16 @@ const puppeteer = require('puppeteer-core');
 		i.clip.y += i.scroll[1];
 
 		// take screenshot
+		let saveTo = __dirname + '/gallery/' + i.saveAs;
 		await page.screenshot({
-			path: saveTo + i.saveAs,
+			path: saveTo,
 			fullPage: false,
 			clip: i.clip
 		});
+		console.log(saveTo);
 	}
-
-	console.log(await browser.version());
 
 	// close the browser
 	await browser.close();
+	console.log('Done.');
 })();

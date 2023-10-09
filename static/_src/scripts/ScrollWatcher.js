1251f9a9e9770032c8b3929e200ae8a4606f2383
amekusa <hello@amekusa.com>
2023-10-10 02:30:45 +0900

    [js:ScrollWatcher::watch] Support `dispatchFirst` param

--- static/_src/scripts/ScrollWatcher.js
+++ static/_src/scripts/ScrollWatcher.js
@@ -3,17 +3,27 @@
  * @author amekusa
  */
 class ScrollWatcher {
+	/**
+	 * @param {Element} target=window
+	 */
 	constructor(target = window) {
 		this.target = target;
-		this.handlers = [];
+		this.tasks = [];
 	}
+	/**
+	 * Registers a callback
+	 * @param {function} fn
+	 */
 	onScroll(fn) {
-		this.handlers.push(fn);
+		this.tasks.push(fn);
 	}
-	watch() {
+	/**
+	 * Starts watching scroll event
+	 * @param {bool} dispatchFirst=false - If true, dispatches the tasks at once first
+	 */
+	watch(dispatchFirst = false) {
 		let c = { // context
 			ev: null,
-			elem: null,
 			curr: { x: 0, y: 0, time: 0 },
 			prev: { x: 0, y: 0, time: 0 },
 			diff: { x: 0, y: 0, time: 0 }
@@ -23,24 +33,37 @@ class ScrollWatcher {
 			c.prev.time = c.curr.time;
 			c.curr.time = time;
 			c.diff.time = c.curr.time - c.prev.time;
-			for (let i = 0; i < this.handlers.length; i++) this.handlers[i](c);
+			for (let i = 0; i < this.tasks.length; i++) this.tasks[i](c);
 			busy = false;
 		};
+		let propX, propY;
+		if (this.target === window) {
+			propX = 'scrollX';
+			propY = 'scrollY';
+		} else {
+			propX = 'scrollLeft';
+			propY = 'scrollTop';
+		}
 		this.target.addEventListener('scroll', ev => {
 			if (busy) return;
 			busy = true;
 			c.ev = ev;
-			c.elem = ev.target.scrollingElement;
 			c.prev.x = c.curr.x;
 			c.prev.y = c.curr.y;
-			c.curr.x = c.elem.scrollLeft;
-			c.curr.y = c.elem.scrollTop;
+			c.curr.x = this.target[propX];
+			c.curr.y = this.target[propY];
 			c.diff.x = c.curr.x - c.prev.x;
 			c.diff.y = c.curr.y - c.prev.y;
-			if (c.diff.x || c.diff.y) {
-				window.requestAnimationFrame(tick);
-			}
+			if (c.diff.x || c.diff.y) window.requestAnimationFrame(tick);
+			else busy = false;
 		});
+		if (dispatchFirst) {
+			// mimicking 'scroll' event
+			c.curr.x = c.prev.x = this.target[propX];
+			c.curr.y = c.prev.y = this.target[propY];
+			c.curr.time = performance.now();
+			tick(c.curr.time);
+		}
 	}
 }
 

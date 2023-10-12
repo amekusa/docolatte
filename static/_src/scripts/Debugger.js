7b8fc64a81c4bfc69139f882155758f99e5d1d51
amekusa <hello@amekusa.com>
2023-10-12 18:58:00 +0900

    [js] Add Debugger class

---
+++ static/_src/scripts/Debugger.js
@@ -0,0 +1,31 @@
+/**
+ * Debugging Utility
+ */
+class Debugger {
+	constructor(label = '[DEBUG]', enabled = true) {
+		this.label = label;
+		this.enabled = enabled;
+	}
+	enable() {
+		this.enabled = true;
+		return this;
+	}
+	disable() {
+		this.enabled = false;
+		return this;
+	}
+	log(...args) {
+		if (!this.enabled) return false;
+		return console.debug(this.label, ...args);
+	}
+	slow(weight) {
+		if (!this.enabled) return false;
+		console.time(this.label + ' slow');
+		let r = 0;
+		for (let i = Math.pow(weight, 7); i >= 0; i--) r += Math.atan(i) * Math.tan(i);
+		console.timeEnd(this.label + ' slow');
+		return r;
+	}
+}
+
+export default Debugger;

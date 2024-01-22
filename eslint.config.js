d7d7e28bc2c139a1e99415dbf183b6b14dc8f5de
amekusa <hello@amekusa.com>
2024-01-22 17:24:34 +0900

    [chore] Add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+module.exports = [
+	{
+		ignores: [
+			'node_modules/',
+			'static/',
+			'docs/'
+		]
+	},
+	{
+		files: [
+			'lib/**/*/js',
+			'src/**/*.js'
+		],
+		rules: {
+			semi: ['error', 'always'],
+			indent: ['warn', 'tab', {
+				ignoredNodes: ['SwitchCase']
+			}]
+		}
+	}
+];
